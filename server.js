668ae6660cfd1b3779a0e4544b07a4b6cb3898a9
Bob Lane <blane2245@gmail.com>
2023-06-18 14:16:37 +0000

    alpha production relase

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
-const path = require("path");
 const bodyParser = require ('body-parser');
 var bcrypt = require("bcryptjs");
 require('dotenv').config();
@@ -10,6 +9,7 @@ require('dotenv').config();
 
 var corsOptions = { origin: "*"};
 
+const env = process.env.NODE_ENV || "development";
 const app= express();
 app.use(cors(corsOptions));
 
@@ -32,29 +32,31 @@ require("./app/routes/todo.routes")(app);
 // set up the listener
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`server is running on port ${port}.`);
+  // console.log(`server is running on port ${port}.`);
 });
 
 // load the db models and sync
 
 const db = require("./app/models");
-db.sequelize.sync ({ force: true }).then(() => {
-  console.log("connected to data base.");
+const force = (env === "development")?true: false
+db.sequelize.sync ({ force: force }).then(() => {
+  // console.log("connected to data base.");
 
   // create the default admin user and the roles
-  initial(db);
+  if (env === 'development')
+    initial(db);
 });
 
 
 // catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  next(createError(404, 'Page not found: ' + req.url ));
-//});
+app.use(function(req, res, next) {
+ return res.status(404).send("That function is not implemented");
+});
   
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  console.log(err);
+  // console.log(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -73,7 +75,7 @@ async function initial (db) {
   db.ROLES.forEach (async function (rolename) {
     const role = await Role.create({name: rolename});
     roles.push(role);
-    console.log("created Role:", JSON.stringify(role, null, 2));
+    // console.log("created Role:", JSON.stringify(role, null, 2));
   });
   
   const user = await User.create({
@@ -81,11 +83,11 @@ async function initial (db) {
     email: "blane2245@gmail.com",
     password: bcrypt.hashSync("root", 8)
   })
-  console.log("created User:", JSON.stringify(user, null, 2) );
+  // console.log("created User:", JSON.stringify(user, null, 2) );
   
   // make root have all roles
   const result = await user.setRoles(roles);
-  console.log("added all roles to root");
+  // console.log("added all roles to root");
 }
 
   
