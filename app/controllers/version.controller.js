5f3d07543369478721057cd2b87c8a70d7039119
Bob Lane <blane2245@gmail.com>
2023-06-28 21:07:29 +0000

    added version release text

--- app/controllers/version.controller.js
+++ app/controllers/version.controller.js
@@ -1,12 +1,14 @@
 const package = require("../../package.json");
+const fs = require("fs");
 
 // list all users
+//TODO need to convert \n to paragraphs and \t to tabs
 exports.getVersion = async (req, res, next) => {
   try 
-	{
-		res.status(200).send(package.version)
+	{	
+		const releaseText = fs.readFileSync("release.txt", "utf8");
+		res.status(200).send("lanedb version:" + package.version + "\n" + releaseText);
 	} catch (error) {
-		res.status(500).send(error.message);
+		res.status(200).send("lanedb version:" + package.version);
 	}
 };
-
