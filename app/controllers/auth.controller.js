9fce445e99f457224ef5d6a723dd27bc89c9fc4e
rlane85 <ryanlane1985@gmail.com>
2023-07-07 21:43:06 +0000

    format

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const config = require("../config/auth.config");
 const { authJwt } = require("../middleware");
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 const User = db.user;
 const Role = db.role;
 
@@ -11,10 +11,9 @@ var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
 exports.signup = async (req, res, next) => {
-
   // Save User to Database if all is
   try {
-    const userName =req.body.username;
+    const userName = req.body.username;
     const password = req.body.password;
     const user = await User.create({
       username: userName,
@@ -24,25 +23,28 @@ exports.signup = async (req, res, next) => {
 
     // user role assigned to new users
     const result = user.setRoles([1]);
-    if (result) res.status(200).send({msg: "User registered successfully!"});
+    if (result) res.status(200).send({ msg: "User registered successfully!" });
   } catch (error) {
-    res.status(500).send({msg: error.message});
+    res.status(500).send({ msg: error.message });
   }
-}
-
+};
 
 exports.signin = [
-  
   // validate username and password
-  body('username', "'username' is required.").trim().isLength({min: 1}).escape(),
-  body('password', "'password' is required.").trim().isLength({min: 1}).escape(),
+  body("username", "'username' is required.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "'password' is required.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   async (req, res, next) => {
     try {
       const errors = validationResult(req).array();
-      const userName =req.body.username;
+      const userName = req.body.username;
       const password = req.body.password;
       if (errors.length == 0) {
-
         // find the user record
         const user = await User.findOne({
           where: {
@@ -51,40 +53,43 @@ exports.signin = [
         });
 
         if (!user) {
-          return res.status(400).send({msg: "User '"+userName+"' not found."});
+          return res
+            .status(400)
+            .send({ msg: "User '" + userName + "' not found." });
         }
 
         // validate the password
         if (!bcrypt.compareSync(password, user.password))
-          return res.status(400).send({msg: "Invalid Password!"});
+          return res.status(400).send({ msg: "Invalid Password!" });
 
         // get the user toke and register it and the userId in the session
-        const token = jwt.sign({ id: user.id },
-                              config.secret,
-                              {
-                                algorithm: 'HS256',
-                                allowInsecureKeySizes: true,
-                                expiresIn: 86400, // 24 hours
-                              });
+        const token = jwt.sign({ id: user.id }, config.secret, {
+          algorithm: "HS256",
+          allowInsecureKeySizes: true,
+          expiresIn: 86400, // 24 hours
+        });
 
         req.session.token = token;
         req.session.userId = user.id;
 
-        return res.status(200).send({msg: "User '"+user.username+"' signed in."});
+        return res
+          .status(200)
+          .send({ msg: "User '" + user.username + "' signed in." });
       } else {
-          return res.status(400).send({msg: "'username' and 'password' must be provided!"});
+        return res
+          .status(400)
+          .send({ msg: "'username' and 'password' must be provided!" });
       }
-
     } catch (error) {
-      return res.status(500).send({msg: error.message});
+      return res.status(500).send({ msg: error.message });
     }
-  }
-]
+  },
+];
 
 exports.signout = async (req, res) => {
   try {
     req.session = null;
-    return res.status(200).send({msg: "You've been signed out!"});
+    return res.status(200).send({ msg: "You've been signed out!" });
   } catch (error) {
     this.next(err);
   }
